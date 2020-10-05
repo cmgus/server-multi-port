39cea974422d3473af714de616b64cf8bd18ee59
Gustavo CM <gustavo.alocoma@gmail.com>
2020-10-04 19:45:08 -0500

    fixed url request

--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 const app = (require('express'))()
 const fetch = require('node-fetch')
 app.get('/', async (req, res) => {
-    const body = await (await fetch(`http://localhost:${process.env.PORT || 3001}`)).json()
+    const body = await (await fetch(`http://localhost:${(parseInt(process.env.PORT) + 1) || 3001}`)).json()
     res.json({ message: body.message })
 })
 
