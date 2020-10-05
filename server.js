df456968d68f2e492ec501dd0e703c684694a921
Gustavo CM <gustavo.alocoma@gmail.com>
2020-10-04 19:28:01 -0500

    add bin files

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const app = (require('express'))()
 
-const port = (+process.env.PORT + 1) || 3001
-
 app.get('/', (req, res) => {
     res.json({ message: 'Server App'})
 })
 
-app.listen(port, () => {
-    console.log(`server lifted port ${port}`);
-})
+module.exports = app
--- webserver.js
+++ webserver.js
@@ -1,11 +1,7 @@
 const app = (require('express'))()
 
-const port = process.env.PORT || 3000
-
 app.get('/', (req, res) => {
     res.json({ message: 'Web Server App'})
 })
 
-app.listen(port, () => {
-    console.log(`web server lifted ${port}`);
-})
+module.exports = app
